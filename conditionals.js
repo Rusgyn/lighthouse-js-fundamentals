fb8e883d34fd981cba00310384cc61158172a394
rusgyn <rusgynmagsanay@gmail.com>
2023-07-17 14:44:28 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -13,13 +13,42 @@
 
 // // console.log ("Now you're ready to go outside!");
 
-const temperature = 12;
+//---------------------------------
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// const temperature = 12;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+// console.log ("Now you're ready to go outside!");
+
+// --------------------------------
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+// --------------------------------
+
+// const temperature = -40;
+
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea..");
+// }
+
+// --------------------------------
+
+const raining = 0;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
-console.log ("Now you're ready to go outside!");
+
+// --------------------------------
