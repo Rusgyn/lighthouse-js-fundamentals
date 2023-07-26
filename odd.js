bdc4fbbca449a364873ad55c62f98cc8cd0a06f4
rusgyn <rusgynmagsanay@gmail.com>
2023-07-26 16:32:54 -0400

    if number is odd will return true, else false

---
+++ odd.js
@@ -0,0 +1,8 @@
+// This function should take a number as a parameter and return true if it is odd and false otherwise.
+
+const isOdd = function(num){
+  return !(num % 2 === 0);
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
