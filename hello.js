73104e7cf3c378b5f1f40d0e5787717165706a53
rusgyn <rusgynmagsanay@gmail.com>
2023-08-01 15:48:43 -0400

    Greetings by declaring and calling functions

--- hello.js
+++ hello.js
@@ -16,9 +16,21 @@ const sayHelloToConsole = function(name){
 }
 sayHelloToConsole('John');
 
+//---------------------------------------------
+
 //return
 const returnSayHello = function(name) {
   return "Hello, " + name;
 }
+// add additional variable to practice
+
+const today = function(hi) {
+  return "\n" + "Hi " + hi + ". How are you?. I am glad to finally meet you!"
+}
+
 const greeting = returnSayHello('James');
-console.log(greeting);
+const glad = today('Gaby');
+console.log((greeting) + (glad)); 
+
+/* RESULT: Hello, James
+Hi Gaby. How are you?. I am glad to finally meet you!*/
