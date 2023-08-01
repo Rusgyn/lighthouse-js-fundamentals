10040032faac7c0367e7f65b752a89964904261d
rusgyn <rusgynmagsanay@gmail.com>
2023-08-01 15:50:48 -0400

    Available ice cream flavours using array.

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer"); //Add the flavour "root beer" to the array without modifying the original array.
+console.log(iceCreamFlavours); //Then console.log the value of the entire array.
+console.log(iceCreamFlavours[0]); // Someone wants the first flavour of ice cream.
+console.log(iceCreamFlavours[5]); // Someone wants the last flavour of ice cream.
+console.log(iceCreamFlavours.length); // Someone wants to know how many options there are.
