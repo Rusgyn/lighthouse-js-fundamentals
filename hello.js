06d008d0c76ce33d8c69120e3e2b1315c834cd25
rusgyn <rusgynmagsanay@gmail.com>
2023-09-25 13:18:27 -0400

    Added more practice in declaring and calling functions. Differentiate between Console.log and Return.

--- hello.js
+++ hello.js
@@ -13,3 +13,35 @@ Hello, Gabby!
 Hello, Dean!
 Hello, Aury!
 */
+
+// difference between return and console.log
+
+const sayHelloToConsole = function (name) {
+   console.log("Hello, " + name);
+};
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name + "!";
+};
+const greeting = returnSayHello("John");
+console.log(greeting);
+
+// Practice:
+
+const returnPracticeHello = function (nameFirst) {
+  return "Hello, " + nameFirst + "! How are you today?";
+};
+const firstGreeting = returnPracticeHello("Dean Aurelius");
+
+const returnPracticeHi = function (nameSecond) {
+  return "\nHi, " + nameSecond + ". I am doing well, and I hope you are too!";
+};
+const secondGreeting = returnPracticeHi("Gabrielle");
+
+console.log(firstGreeting + secondGreeting);
+
+/* OUTPUT:
+Hello, Dean Aurelius! How are you today?
+Hi, Gabrielle. I am doing well, and I hope you are too!
+*/
